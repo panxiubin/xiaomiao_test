76118b601539d519d74a0dd51910d92eefe09315
xiubinpan@gmail.com <xiubinpan@gmail.com>
2016-10-31 11:50:33 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |i|
+  Student.create(name: "S-#{i+1}")
+  Teacher.create(name: "T-#{i+1}")
+  Course.create(name: "C-#{i+1}", teacher_id: i+1)
+
+  ScRelation.create(student_id: i+1, course_id: i+1)
+  StRelation.create(student_id: i+1, teacher_id: i+1)
+end
